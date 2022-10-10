74f9bd39ac73744b68a2c8c5d0152b1cffa99b70
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2022-10-10 12:19:53 +0800

    Add files via upload

--- weiw/mods/mc_setskin.php
+++ weiw/mods/mc_setskin.php
@@ -10,10 +10,10 @@
 				$attribute = json_decode(http::open("https://littleskin.cn/texture/{$_GET['id']}"),true);
 				if(isset($attribute['hash']))
 				{
-					mc::skin($user['id'],http::open("https://littleskin.cn/textures/{$attribute['hash']}"));
-					throw new Exception('使用成功');
+					mc::skin($user['id'],$attribute['hash']);
+					return Array('error' => 'ok','hash' => $attribute['hash']);
 				}
-				else throw new Exception('使用失败');
+				else throw new Exception('获取不到hash值');
 			}
 			else throw new Exception('缺少参数');
 		}
