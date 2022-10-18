0b5d729d4c66ae41a3507c7ca43738abe1a86e54
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2022-10-18 19:41:49 +0800

    Add files via upload

--- weiw/fc/class mc_auth.php
+++ weiw/fc/class mc_auth.php
@@ -221,22 +221,84 @@
 							header('Content-Type: image/png');
 							die($data);
 						}
-						
-						http_response_code(404);
-						die();
 					}
-					catch(error $error)
-					{
-						http_response_code(404);
-						die();
-					}
+					catch(error $error){}
 				}
+			}
+			
+			
+			http_response_code(404);
+			die();
+		}
+		
+		
+		//查询头像
+		if(isset($_SERVER['PATH_INFO']) && preg_match('!^/avatar(\?.*)?$!',$_SERVER['PATH_INFO']))
+		{
+			$output = 'iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAFDUlEQVR42u2a20sUURzH97G0LKMotPuWbVpslj1olJ';
+			$output .= 'XdjCgyisowsSjzgrB0gSKyC5UF1ZNQWEEQSBQ9dHsIe+zJ/+nXfM/sb/rN4ZwZ96LOrnPgyxzP/M7Z+X7OZc96JpE';
+			$output .= 'ISfWrFhK0YcU8knlozeJKunE4HahEqSc2nF6zSEkCgGCyb+82enyqybtCZQWAzdfVVFgBJJNJn1BWFgC49/VpwGVl';
+			$output .= 'D0CaxQiA5HSYEwBM5sMAdKTqygcAG9+8coHKY/XXAZhUNgDYuBSPjJL/GkzVVhAEU5tqK5XZ7cnFtHWtq/TahdSw2';
+			$output .= 'l0HUisr1UKIWJQBAMehDuqiDdzndsP2EZECAG1ZXaWMwOCODdXqysLf++uXUGv9MhUHIByDOijjdiSAoH3ErANQD7';
+			$output .= '3C7TXXuGOsFj1d4YH4OTJAEy8y9Hd0mCaeZ5z8dfp88zw1bVyiYhCLOg1ZeAqC0ybaDttHRGME1DhDeVWV26u17lR';
+			$output .= 'APr2+mj7dvULfHw2q65fhQRrLXKDfIxkau3ZMCTGIRR3URR5toU38HbaPiMwUcKfBAkoun09PzrbQ2KWD1JJaqswj';
+			$output .= 'deweoR93rirzyCMBCmIQizqoizZkm2H7iOgAcHrMHbbV9KijkUYv7qOn55sdc4fo250e+vUg4329/Xk6QB/6DtOws';
+			$output .= '+dHDGJRB3XRBve+XARt+4hIrAF4UAzbnrY0ve07QW8uHfB+0LzqanMM7qVb+3f69LJrD90/1axiEIs6qIs21BTITo';
+			$output .= 'ewfcSsA+Bfb2x67OoR1aPPzu2i60fSNHRwCw221Suz0O3jO+jh6V1KyCMGse9721XdN5ePutdsewxS30cwuMjtC86';
+			$output .= '0T5JUKpXyKbSByUn7psi5l+juDlZYGh9324GcPKbkycaN3jUSAGxb46IAYPNZzW0AzgiQ5tVnzLUpUDCAbakMQXXr';
+			$output .= 'OtX1UMtHn+Q9/X5L4wgl7t37r85OSrx+TYl379SCia9KXjxRpiTjIZTBFOvrV1f8ty2eY/T7XJ81FQAwmA8ASH1ob';
+			$output .= '68r5PnBsxA88/xAMh6SpqW4HRnLBrkOA9Xv5wPAZjAUgOkB+SHxgBgR0qSMh0zmZRsmwDJm1gFg2PMDIC8/nAHIMl';
+			$output .= 's8x8GgzOsG5WiaqREgYzDvpTwjLDy8NM15LpexDEA3LepjU8Z64my+8PtDCmUyRr+fFwA2J0eAFYA0AxgSgMmYBMZ';
+			$output .= 'TwFQnO9RNAEaHOj2DXF5UADmvAToA2ftyxZYA5BqgmZZApDkdAK4mAKo8GzPlr8G8AehzMAyA/i1girUA0HtYB2Ca';
+			$output .= 'IkUBEHQ/cBHSvwF0AKZFS5M0ZwMQtEaEAmhtbSUoDADH9ff3++QZ4o0I957e+zYAMt6wHkhzpjkuAcgpwNcpA7AZD';
+			$output .= 'LsvpwiuOkBvxygA6Bsvb0HlaeKIF2EbADZpGiGzBsA0gnwQHGOhW2snRpbpPexbAB2Z1oicAMQpTnGKU5ziFKc4xS';
+			$output .= 'lOcYpTnOIUpzgVmgo+XC324WfJAdDO/+ceADkCpuMFiFKbApEHkOv7BfzfXt+5gpT8V7rpfYJcDz+jAsB233r6yyB';
+			$output .= 'sJ0mlBCDofuBJkel4vOwBFPv8fyYAFPJ+wbSf/88UANNRVy4Awo6+Ig2gkCmgA5DHWjoA+X7AlM//owLANkX0w035';
+			$output .= '9od++pvX8fdMAcj3/QJ9iJsAFPQCxHSnQt8vMJ3v2wCYpkhkAOR7vG7q4aCXoMoSgG8hFAuc/grMdAD4B/kHl9da7';
+			$output .= 'Ne9AAAAAElFTkSuQmCC';
+			$output = base64_decode($output);
+			
+			
+			if(isset($_GET['size']) && $_GET['size'] >= 1 && $_GET['size'] <= 255)
+			{
+				$size = $_GET['size'];	
 			}
 			else
 			{
-				http_response_code(404);
-				die();
+				$size = 48;
 			}
+			
+			if(isset($_GET['hash']))
+			{
+				if(file_exists($texture = __MKHDIR__."/user_textures/{$_GET['hash']}.png"))
+				{
+					$output = file_get_contents($texture);
+				}
+				else
+				{
+					try
+					{
+						if($output = file_get_contents("https://littleskin.cn/textures/{$_GET['hash']}"))
+						{
+							file_put_contents($texture,$output);
+						}
+					}
+					catch(error $error){}
+				}
+			}
+
+			$im = imagecreatefromstring($output);
+			$av = imagecreatetruecolor($size, $size);
+
+			imagecopyresized($av, $im, 0, 0, 8, 8, $size, $size, 8, 8);
+			imagecolortransparent($im, imagecolorat($im, 63, 0));
+			imagecopyresized($av, $im, 0, 0, 8 + 32, 8, $size, $size, 8, 8);
+
+			header('Content-type: image/png');
+			imagepng($av);
+			imagedestroy($im);
+			imagedestroy($av);
+			die();
 		}
 		
 		
