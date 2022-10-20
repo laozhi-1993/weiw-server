b9b26a8c58fa06ccab745fa44bae74c6c6405be8
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2022-10-21 03:42:41 +0800

    Add files via upload

--- weiw/mods/mc_set_texture.php
+++ weiw/mods/mc_set_texture.php
@@ -13,24 +13,30 @@
 					if($texture['type'] == 'steve')
 					{
 						mc::skin($user['id'],$texture['hash']);
-						return Array('error' => 'ok','hash' => $texture['hash']);
+						return Array('error' => 'ok','hash' => $texture['hash'],'type' => 'steve');
 					}
 					
 					if($texture['type'] == 'alex')
 					{
 						mc::skin($user['id'],$texture['hash'],true);
-						return Array('error' => 'ok','hash' => $texture['hash']);
+						return Array('error' => 'ok','hash' => $texture['hash'],'type' => 'alex');
+					}
+					
+					if($texture['type'] == 'cape' && mc::admin())
+					{
+						mc::cape($user['id'],$texture['hash'],true);
+						return Array('error' => 'ok','hash' => $texture['hash'],'type' => 'cape');
 					}
 					
 					throw new Exception('不支持的材质类型');
 				}
-				else throw new Exception('获取材质信息失败');
+				else throw new Exception('缺少关键参数');
 			}
-			else throw new Exception('缺少参数');
+			else throw new Exception('缺少id参数');
 		}
 		catch(error $error)
 		{
-			return Array('error' => '获取材质信息失败');
+			return Array('error' => '无法获取材质信息或材质不存在');
 		}
 		catch(Exception $Exception)
 		{
