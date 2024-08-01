fef7f05a94a8642738e4c9f9301fd3c35208e497
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2024-08-01 15:58:36 +0800

    Add files via upload

--- weiw/config/config.php
+++ weiw/config/config.php
@@ -1,8 +1,8 @@
 <?php return array (
-  'serverName' => 'serverName',
-  'domain' => 'example.com',
+  'serverName' => 'laozhi',
+  'domain' => 'laozhi.cc',
   'download' => '',
-  'authUrl' => 'https://example.com/weiw/index_auth.php',
+  'authUrl' => 'http://127.0.0.1/weiw/index_auth.php',
   'checkinMoney' => '1000',
   'defaultMoney' => '1000',
 ) ?>
