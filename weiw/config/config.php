bfaed141d12349fa408229e4f46ed94b81b1a1f8
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2025-04-19 02:00:56 +0800

    Update config.php

--- weiw/config/config.php
+++ weiw/config/config.php
@@ -2,7 +2,7 @@
   'serverName' => 'example',
   'domain' => 'example.com',
   'download' => '',
-  'authUrl' => 'http://127.0.0.1/weiw/index_auth.php',
+  'authUrl' => '',
   'checkinMoney' => '1000',
   'defaultMoney' => '1000',
 ) ?>
