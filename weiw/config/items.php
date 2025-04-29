2b6faa7d80a2af87a8b48e37130ef003f6287de8
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2025-04-29 23:42:58 +0800

    Add files via upload

--- weiw/config/items.php
+++ weiw/config/items.php
@@ -1,19 +1,19 @@
 <?php return '{
-    "移速加10，持续时间60秒":{"price":"100","command":"effect give \'{name}\' minecraft:speed 60 10"}
+    "移速加10，持续时间60秒":{"price":"100","icon":"/images/001.svg","command":"effect give \'{name}\' minecraft:speed 60 10"}
     ,
-    "力量加10，持续时间120秒":{"price":"100","command":"effect give \'{name}\' minecraft:strength 120 10"}
+    "力量加10，持续时间120秒":{"price":"100","icon":"/images/002.svg","command":"effect give \'{name}\' minecraft:strength 120 10"}
     ,
-    "跳跃加10，持续时间60秒":{"price":"100","command":"effect give \'{name}\' minecraft:jump_boost 60 10"}
+    "跳跃加10，持续时间60秒":{"price":"100","icon":"/images/003.svg","command":"effect give \'{name}\' minecraft:jump_boost 60 10"}
     ,
-    "血量上限加20个心，持续时间600秒":{"price":"100","command":"effect give \'{name}\' minecraft:absorption 600 9"}
+    "血量上限加20个心，持续时间600秒":{"price":"100","icon":"/images/004.svg","command":"effect give \'{name}\' minecraft:absorption 600 9"}
     ,
-    "给予64个烟花火箭":{"price":"1000","command":"give {name} firework_rocket 64"}
+    "立即天亮":{"price":"100","icon":"/images/005.svg","command":"time set 1"}
     ,
-    "向所有在线玩家广播你是个大土豪":{"price":"10000","command":"title @a title {\\"text\\":\\"{name}是个大土豪\\"}"}
+    "立即晴天":{"price":"100","icon":"/images/006.svg","command":"weather clear"}
     ,
-    "炽燃余烬":{"price":"1000","command":"give {name} cataclysm:burning_ashes"}
+    "给予64个烟花火箭":{"price":"1000","icon":"/images/000.svg","command":"give {name} firework_rocket 64"}
     ,
-    "立即天亮":{"price":"100","command":"time set 1"}
+    "向所有在线玩家广播你是个大土豪":{"price":"10000","icon":"/images/000.svg","command":"title @a title {\\"text\\":\\"{name}是个大土豪\\"}"}
     ,
-    "立即晴天":{"price":"100","command":"weather clear"}
+    "炽燃余烬":{"price":"1000","icon":"/images/000.svg","command":"give {name} cataclysm:burning_ashes"}
 }' ?>
