9ea0a037acadcff67ebae2414fc6f1a43c655665
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2025-05-19 21:25:42 +0800

    Add files via upload

--- weiw/mods/mc_client_data.php
+++ weiw/mods/mc_client_data.php
@@ -1,95 +1,95 @@
 <?php return function ()
-{
-	try
 	{
-		// 获取当前用户
-		$user = mc_user_authentication::getUser();
-		if (!$user) {
-			throw new Exception('没有登录');
-		}
-
-		// 获取客户端配置
-		$client = config::loadConfig('client');
-		$client = reset($client);
-
-		if (!$client) {
-			return false;
-		}
-
-		// 获取用户数据
-		$userData = $user->toArray();
-
-		// 定义目录路径
-		$file_handler = new file_handler("{$_SERVER['DOCUMENT_ROOT']}/files/{$client['name']}");
-
-		if (!$file_handler->isExists()) {
-			mkdir($file_handler->getFullPath(), 0777, true);
-		}
-
-		$clientData = Array();
-		$clientData['username']       = $userData['name'];
-		$clientData['uuid']           = $userData['uuid'];
-		$clientData['accessToken']    = $userData['accessToken'];
-
-		$clientData['id']             = $client['id'];
-		$clientData['name']           = $client['name'];
-		$clientData['version']        = $client['version'];
-		$clientData['extensionType']  = $client['extensionType'];
-		$clientData['extensionValue'] = $client['extensionValue'];
-		$clientData['address']        = $client['address'];
-		$clientData['authModule']     = $client['authModule'];
-		$clientData['jvm']            = $client['jvm'];
-
-		$clientData['mods']           = [];
-		$clientData['downloads']      = [];
-
-
-
-		foreach(preg_split('!\r\n|\n|\r!', $client['downloads']) as $download)
+		try
 		{
-			if ($download && preg_match('!(?:\[(.+)\])?(https?://.+)!i', $download, $file))
-			{
-				if ($file[1])
-				{
-					$filePath = $file[1] . '/' . urldecode(basename($file[2]));
-					$fileTime = 0;
-				}
-				else
-				{
-					$filePath = 'mods/' . urldecode(basename($file[2]));
-					$fileTime = 0;
-				}
+			// 获取当前用户
+			$user = mc_user_authentication::getUser();
+			if (!$user) {
+				throw new Exception('没有登录');
+			}
 
+			// 获取客户端配置
+			$client = config::loadConfig('client');
+			$client = reset($client);
+
+			if (!$client) {
+				return false;
+			}
+
+			// 获取用户数据
+			$userData = $user->toArray();
+
+			// 定义目录路径
+			$file_handler = new file_handler("{$_SERVER['DOCUMENT_ROOT']}/files/{$client['name']}");
+
+			if (!$file_handler->isExists()) {
+				mkdir($file_handler->getFullPath(), 0777, true);
+			}
+
+			$clientData = Array();
+			$clientData['username']       = $userData['name'];
+			$clientData['uuid']           = $userData['uuid'];
+			$clientData['accessToken']    = $userData['accessToken'];
+
+			$clientData['id']             = $client['id'];
+			$clientData['name']           = $client['name'];
+			$clientData['version']        = $client['version'];
+			$clientData['extensionType']  = $client['extensionType'];
+			$clientData['extensionValue'] = $client['extensionValue'];
+			$clientData['address']        = $client['address'];
+			$clientData['authModule']     = $client['authModule'];
+			$clientData['jvm']            = $client['jvm'];
+
+			$clientData['mods']           = [];
+			$clientData['downloads']      = [];
+
+
+
+			foreach(preg_split('!\r\n|\n|\r!', $client['downloads']) as $download)
+			{
+				if ($download && preg_match('!(?:\[(.+)\])?(https?://.+)!i', $download, $file))
+				{
+					if ($file[1])
+					{
+						$filePath = $file[1] . '/' . rawurldecode(basename($file[2]));
+						$fileTime = 0;
+					}
+					else
+					{
+						$filePath = 'mods/' . rawurldecode(basename($file[2]));
+						$fileTime = 0;
+					}
+
+					$clientData['downloads'][] = [
+						'url'  => $file[2],
+						'path' => $filePath,
+						'time' => $fileTime,
+					];
+				}
+			}
+
+			foreach ($file_handler->getFilesRecursively() as $file)
+			{
 				$clientData['downloads'][] = [
-					'url'  => $file[2],
-					'path' => $filePath,
-					'time' => $fileTime,
+					'url'  => $file[1],
+					'path' => $file[2],
+					'time' => $file[0],
 				];
 			}
-		}
 
-		foreach ($file_handler->getFilesRecursively() as $file)
-		{
-			$clientData['downloads'][] = [
-				'url'  => $file[1],
-				'path' => $file[2],
-				'time' => $file[0],
-			];
-		}
-
-		foreach($clientData['downloads'] as $fileDownload)
-		{
-			if (preg_match('!^mods/.*jar$!i', $fileDownload['path'] ?? ''))
+			foreach($clientData['downloads'] as $fileDownload)
 			{
-				$clientData['mods'][] = basename($fileDownload['path']);
+				if (preg_match('!^mods/.*jar$!i', $fileDownload['path'] ?? ''))
+				{
+					$clientData['mods'][] = basename($fileDownload['path']);
+				}
 			}
+
+
+			return $clientData;
 		}
-
-
-		return $clientData;
-	}
-	catch (Exception $Exception)
-	{
-		return ['error' => $Exception->getMessage()];
-	}
-};
+		catch (Exception $Exception)
+		{
+			return ['error' => $Exception->getMessage()];
+		}
+	};
--- weiw/mods/mc_server_data.php
+++ weiw/mods/mc_server_data.php
@@ -1,42 +1,57 @@
 <?php return function ()
-{
-	try
 	{
-		$serverData = Array();
-		$serverData['mods'] = [];
+		try
+		{
+			$serverData = Array();
+			$serverData['downloads'] = [];
+			$serverData['mods'] = [];
 
-		// 获取客户端配置
-		$client = config::loadConfig('client');
-		$client = reset($client);
+			// 获取客户端配置
+			$client = config::loadConfig('client');
+			$client = reset($client);
 
-		if (!$client) {
+			if (!$client) {
+				return $serverData;
+			}
+
+			// 定义目录路径
+			$file_handler = new file_handler("{$_SERVER['DOCUMENT_ROOT']}/files/{$client['name']}/mods");
+
+
+			foreach(preg_split('!\r\n|\n|\r!', $client['downloads']) as $download)
+			{
+				if ($download && preg_match('!(?:\[(.+)\])?(https?://.+)!i', $download, $file))
+				{
+					$file[1] || $serverData['downloads'][] = [
+						'url' => $file[2],
+						'path' => 'mods/' . rawurldecode(basename($file[2])),
+					];
+				}
+			}
+
+			foreach ($file_handler->getDirectoryListing() as $file)
+			{
+				if (strpos($file['name'], '[c]') !== 0)
+				{
+					$serverData['downloads'][] = [
+						'url' => $file['url'],
+						'path' => 'mods/' . $file['name'],
+					];
+				}
+			}
+
+			foreach($serverData['downloads'] as $fileDownload)
+			{
+				if (preg_match('!^mods/.*jar$!i', $fileDownload['path'] ?? ''))
+				{
+					$serverData['mods'][] = basename($fileDownload['path']);
+				}
+			}
+
 			return $serverData;
 		}
-
-		// 定义目录路径
-		$file_handler = new file_handler("{$_SERVER['DOCUMENT_ROOT']}/files/{$client['name']}/mods");
-
-
-		foreach(preg_split('!\r\n|\n|\r!', $client['downloads']) as $download)
+		catch (Exception $Exception)
 		{
-			if ($download && preg_match('!(?:\[(.+)\])?(https?://.+)!i', $download, $file))
-			{
-				$file[1] || $serverData['mods'][] = $file[2];
-			}
+			return ['error' => $Exception->getMessage()];
 		}
-
-		foreach ($file_handler->getDirectoryListing() as $file)
-		{
-			if (strpos($file['name'], '[c]') !== 0)
-			{
-				$serverData['mods'][] = $file['url'];
-			}
-		}
-
-		return $serverData;
-	}
-	catch (Exception $Exception)
-	{
-		return ['error' => $Exception->getMessage()];
-	}
-};
+	};
