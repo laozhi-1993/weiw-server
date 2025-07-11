e4d07b5d126e5fa058cf4f743ca3e346cee9aebf
laozhi-1993 <92775292+laozhi-1993@users.noreply.github.com>
2025-07-11 19:17:09 +0800

    Delete weiw/config directory

--- weiw/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-  'name' => 'example',
-  'domain' => 'example.com',
-  'download' => '',
-  'authServerUrl' => '',
-  'checkinMoney' => '1000',
-  'defaultMoney' => '1000',
-); ?>
